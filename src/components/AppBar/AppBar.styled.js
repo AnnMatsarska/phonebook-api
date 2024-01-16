03bce7f052ad264b3a0195f84533ddb82d94ff06
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-01-16 15:17:04 +0200

    Update AppBar.styled.js

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -5,8 +5,6 @@ export const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
   padding: 25px;
-  // outline: 2px solid red;
-  // outline-offset: -2px;
 `;
 export const HeaderNav = styled.header`
   display: flex;
